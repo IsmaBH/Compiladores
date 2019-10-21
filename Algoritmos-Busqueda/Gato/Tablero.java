d289a6c5286a77d5278dae61147be93ad7b0dbff
IsmaBH <deigo-b@hotmail.com>
2019-10-20 20:45:47 -0500

    Cambios menores

--- Algoritmos-Busqueda/Gato/Tablero.java
+++ Algoritmos-Busqueda/Gato/Tablero.java
@@ -1,9 +1,26 @@
 public class Tablero{
 	//Atributos de la clase
-	int[][] tablero;
-	int turno;
-	public Tablero(){
-		this.tablero = {{0,0,0},{0,0,0},{0,0,0}};
-		this.turno = 0;
+	int[][] matrix;
+	//Constructor de la clase
+	public Tablero(int col,int row){
+		this.matrix = new int[col][row];
+	}
+	//Metodos de la clase
+	//Inicializacion
+	public void initMatriz(){
+		for (int i = 0; i < matrix.length ; i++ ) {
+			for (int j = 0; j < matrix[i].length ; j++ ) {
+				this.matrix[i][j] = 0;
+			}
+		}
+	}
+	//Corrimientos
+	//Siguiente estado de la matriz(En desarrollo)
+	public Matriz nextStat(Matriz mat, int num){
+		for (int i = 0; i < matrix.length ; i++ ) {
+			for (int j = 0; j < matrix[i].length ; j++ ) {
+				matrix[]
+			}
+		}
 	}
 }
