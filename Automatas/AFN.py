c7b2c79e9863e8bdc0dc64c0e85e3483311cd209
IsmaBH <deigo-b@hotmail.com>
2020-10-25 13:22:19 -0600

    Nuevos metodos añadidos Se añaden los metodos de agregar/eliminar transicion tambien los metodos de obtencion de iniciales/finales y los metods para modificar iniciales/finales

--- Automatas/AFN.py
+++ Automatas/AFN.py
@@ -1,5 +1,4 @@
 import re
-import pickle
 class AFN:
 	"""docstring for AFN"""
 	def __init__(self):
@@ -28,9 +27,55 @@ class AFN:
 		archivo.close()
 		archivo = open(nombre,'a')
 		archivo.writelines(self.finales)
+		archivo.write('\n')
 		archivo.writelines(self.AF)
 		archivo.close()
-	def agregar_transicion(self,inicio,final,simbolo):
+	def agregar_transicion(self,inicio,fin,simbolo):
 		#Metodo que agrega una transicion
 		#al AFN cargado en memoria
-		pass
+		transicion = str(inicio)+'->'+str(fin)+','+simbolo+'\n'
+		self.AF.append(transicion)
+	def eliminar_transicion(self,inicio,fin,simbolo):
+		#Metodo que elimina una transicion
+		#al AFN cargado en memoria
+		transicion = str(inicio)+'->'+str(fin)+','+simbolo+'\n'
+		indice = self.AF.index(transicion)
+		self.AF.pop(indice)
+	def obtener_inicial(self):
+		#Metodo que devuelve un entero con
+		#el valor del estado incial
+		#Este metodo busca el estado inicial en la lista
+		#de estados iniciales que es un atributo de la clase
+		#es necesario usar el metodo establecer_inicial
+		#si es que se quiere modificar este valor
+		e1 = int(self.inicial[0])
+		return e1
+	def obtener_finales(self):
+		#Metodo que devuelve una lista con los estados
+		#finales del AF, este valor se busca en la lista
+		#que es atributo de la clase, por lo que es necesario
+		#usar el metodo establecer_final si se quiere modificar
+		ef = self.finales
+		return ef
+	def establecer_inicial(self,estado):
+		#Metodo que dado un estado lo establece como
+		#estado inicial en el AF cargado en memoria
+		try:
+			indice = self.AF.index(estado)
+		except ValueError:
+			print("El estado dado no existe")
+		else:
+			datos_e = re.findall("\d+",estado)
+			self.inicial[0] = datos_e[0]
+			n_inicial = self.AF.pop(indice)
+			self.AF.insert(0,n_inicial)
+	def establecer_final(self,estado):
+		#Metodo que dado un estado lo añade a
+		#la lista de estados finales del AF
+		try:
+			indice = self.AF.index(estado)
+		except ValueError:
+			print("El estado dado no existe")
+		else:
+			datos_e = re.findall("\d+",estado)
+			self.finales.append(datos_e[0])
--- Automatas/main.py
+++ Automatas/main.py
@@ -1,11 +1,45 @@
 #Programa sencillo para probar las clases AFN y AFD
 from AFN import AFN
 
+#Instancia de la clase AFN
 af1 = AFN()
+#Rutas para los metodos de carga y guardado
 ruta = '../Automatas/prueba.af'
 ruta2 = '../Automatas/prueba2.af'
+#Ejecución del metodo cargar
 af1.cargar_desde(ruta)
+#Se muestra que la carga fue exitosa en pantalla
 print(af1.AF)
-print(af1.inicial)
-print(af1.finales)
+#Ejecución del metodo guardar
 af1.guardar_en(ruta2)
+#Ejecución del metodo agregar_transicion
+af1.agregar_transicion(11,12,'E')
+#Se muestra que se agrego la transicion
+print(af1.AF)
+#Ejecucion del metodo eliminar_transicion
+af1.eliminar_transicion(10,11,'b')
+#Mostramos que se elimino la penultima transicion
+print(af1.AF)
+#Ejecución del metodo obtener_inicial
+e_inicial = af1.obtener_inicial()
+#Lo mostramos en pantalla
+print('El estado inicial es '+str(e_inicial))
+#Ejecucion del metodo obtener_finales
+e_finales = af1.obtener_finales()
+#Los mostramos en pantalla
+print('Los estados finales son: ')
+for i in range(len(e_finales)):
+	print(e_finales[i])
+#Ejecucion del metodo establecer_inicial
+af1.establecer_inicial('2->3,E\n')
+#comprobamos que los datos se modificaron correctamente
+print(af1.AF)
+e_inicial = af1.obtener_inicial()
+print('El nuevo estado inicial es '+str(e_inicial))
+#Ejecucion del metodo establecer_final
+af1.establecer_final('19->10,b\n')
+#mostramos en pantalla la nueva lista de finales
+e_finales = af1.obtener_finales()
+print('Los estados finales son: ')
+for i in range(len(e_finales)):
+	print(e_finales[i])
